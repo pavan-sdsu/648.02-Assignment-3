68db3f74e61c76ff3455ae7802ba75f7ebdbcf0a
pavan <pavan1645@gmail.com>
2020-03-01 13:40:58 -0800

    Added apollo backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,39 @@
+const fs = require('fs');
 const express = require("express");
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const path = require("path");
 
 const PORT = 3000;
 
+let PRODUCTS = [{"Category":["Accessories"],"Price":"1123","Name":"Clarinet","Image":"https://via.placeholder.com/350x150","id":1},{"Category":["Shirts"],"Price":"10","Name":"Blue Tshirt","Image":"https://via.placeholder.com/350x150","id":2}]
+
+const resolvers = {
+	Query: {
+		productList: () => PRODUCTS
+	},
+	Mutation: {
+		addProduct: (_, { Category, Price, Name, Image }) => {
+			const PRODUCT = {
+				id: PRODUCTS.length+1,
+				Category: Category,
+				Price: Price,
+				Name: Name,
+				Image: Image
+			}
+			PRODUCTS.push(PRODUCT)
+			return PRODUCT
+		}
+	}
+};
+  
+const server = new ApolloServer({
+	typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+	resolvers
+});
+
+server.applyMiddleware({ app });
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../static")));
 
-app.listen(PORT, () => console.log('Listening on PORT', PORT));
+app.listen(PORT, () => console.log('Listening on PORT', PORT, server.graphqlPath));
